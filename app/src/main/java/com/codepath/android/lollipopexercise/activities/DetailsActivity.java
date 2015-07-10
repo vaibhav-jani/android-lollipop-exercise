dafd5713957267dfd1e62eb6d33cc7829a18f93c
Vaibhav Jani <vaibhavaniljani@gmail.com>
2015-07-10 19:42:30 +0530

    Added pallete example and FAB

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,7 +1,14 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,6 +25,7 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView tvName;
     private TextView tvPhone;
     private View vPalette;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +36,34 @@ public class DetailsActivity extends AppCompatActivity {
         tvName = (TextView) findViewById(R.id.tvName);
         tvPhone = (TextView) findViewById(R.id.tvPhone);
         vPalette = findViewById(R.id.vPalette);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + mContact.getNumber()));
+                startActivity(intent);
+            }
+        });
 
         // Extract contact from bundle
         mContact = (Contact)getIntent().getExtras().getParcelable(EXTRA_CONTACT);
 
         // Fill views with data
         Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(ivProfile);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), mContact.getThumbnailDrawable());
+        // Asynchronous
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            public void onGenerated(Palette p) {
+                vPalette.setBackgroundColor(p.getLightMutedColor(Color.WHITE));
+            }
+        });
+
+        /*Palette p = Palette.from(bitmap).generate();
+        vPalette.setBackgroundColor(p.getLightMutedColor(Color.WHITE));*/
+
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
     }
